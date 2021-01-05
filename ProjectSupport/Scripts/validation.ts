0be79617e51536654209006d2d5d6ebed0c856b9
Sergiu <dina_sergiu@yahoo.com>
2021-01-05 16:11:05 +0200

    finished-demo-page

--- ProjectSupport/Scripts/validation.ts
+++ ProjectSupport/Scripts/validation.ts
@@ -1,1 +1,8 @@
-﻿
+﻿function cbChange(obj: any): void {
+    if (obj.checked) {
+        const cbs = document.getElementsByClassName("cb") as HTMLCollectionOf<any>;
+        for (let i: number = 0; i < cbs.length; i++)
+            cbs[i].checked = false;
+        obj.checked = true;
+    }
+}
