40c719a325c9f9ec66e5c1f585b3c59893917d8b
Sergiu <dina_sergiu@yahoo.com>
2021-01-07 14:55:32 +0200

    change-password

--- ProjectSupport/Areas/Identity/IdentityHostingStartup.cs
+++ ProjectSupport/Areas/Identity/IdentityHostingStartup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,6 +31,8 @@ namespace ProjectSupport.Areas.Identity
                     .AddEntityFrameworkStores<AppDbContext>()
                     .AddDefaultUI()
                     .AddDefaultTokenProviders();
+
+                services.AddTransient<IEmailSender, EmailSender>();
 
                 services.AddAuthentication()
                     .AddGoogle(o =>
