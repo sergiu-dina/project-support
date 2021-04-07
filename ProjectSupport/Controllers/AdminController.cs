ed4edbfdf0b41640795f1c2f4401c463113ac03f
Sergiu <dina_sergiu@yahoo.com>
2021-04-07 16:59:53 +0300

    add-role

--- ProjectSupport/Controllers/AdminController.cs
+++ ProjectSupport/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ProjectSupport.Areas.Identity.Data;
+using ProjectSupport.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,38 @@ namespace ProjectSupport.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            var model = new CreateRoleViewModel();
+            model.Roles= roleManager.Roles.OrderBy(r => r.Name);
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+                IdentityResult result = await roleManager.CreateAsync(identityRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("CreateRole", "Admin");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            model.Roles= roleManager.Roles.OrderBy(r => r.Name);
+            return View(model);
         }
     }
 }
---
+++ ProjectSupport/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSupport.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+        public IEnumerable<IdentityRole> Roles { get; set; }
+    }
+}
