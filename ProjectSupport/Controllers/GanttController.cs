07aba169fd4c83f0c698bdbef2fdff172e4a3b95
Sergiu <dina_sergiu@yahoo.com>
2021-05-08 23:38:54 +0300

    gantt-view

---
+++ ProjectSupport/Controllers/GanttController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSupport.Controllers
+{
+    public class GanttController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Gantt()
+        {
+            return View();
+        }
+    }
+}
